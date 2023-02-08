cca1196a76cecb5d70d4a3a98cef0dedcd8c4873
Kristian Kurtashev <Kristian.Kurtashev@digitall.com>
2023-02-08 02:34:05 +0200

    styling added

--- src/main/java/com/example/logisticcompany/config/WebSecurityConfig.java
+++ src/main/java/com/example/logisticcompany/config/WebSecurityConfig.java
@@ -84,7 +84,7 @@ public class WebSecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/css/**", "/webjars/**");
     }
 
 }
