0a5c54dc7317aca42a250e2b56d7ceea2717fa3f
Viktor Grozev <Boobix99@gmail.com>
2023-02-13 22:46:31 +0200

    Delete logisticcompany directory

--- logisticcompany/repository/CompanyOfficeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.logisticcompany.repository;
-
-import com.example.logisticcompany.model.CompanyOffice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CompanyOfficeRepository extends JpaRepository<CompanyOffice, UUID> {
-}
--- logisticcompany/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.logisticcompany.service;
-
-import com.example.logisticcompany.model.User;
-
-public interface UserService {
-    public User save(User user);
-
-}
