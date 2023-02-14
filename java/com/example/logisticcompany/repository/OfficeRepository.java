3fed7776cc7a32bccafcbe0e3cd9294f3e930400
Antonio Antonov <70547557+DevAntonov@users.noreply.github.com>
2023-02-14 14:23:41 +0200

    Delete java/com/example/logisticcompany directory

--- java/com/example/logisticcompany/repository/OfficeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.logisticcompany.repository;
-
-import com.example.logisticcompany.model.Office;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OfficeRepository extends JpaRepository<Office, UUID> {
-}
